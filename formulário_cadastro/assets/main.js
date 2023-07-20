2f6148e05550c9d15537c01fadff874b298a40e0
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-07-20 20:55:06 -0300

    Alterações

--- formulário_cadastro/assets/main.js
+++ formulário_cadastro/assets/main.js
@@ -3,6 +3,7 @@ const nomeInput = document.querySelector('#nome')
 const sobrenomeInput = document.querySelector('#sobrenome')
 const cpf = document.querySelector('#cpf')
 const senha = document.querySelector('#senha')
+const repetirSenha = document.querySelector('#repetirSenha')
 
 const textName = document.querySelector('.textName')
 const textSobrenome = document.querySelector('.texSobrenome')
@@ -11,7 +12,7 @@ const textUsuario = document.querySelector('.textUsuario')
 const textSenha = document.querySelector('.textSenha')
 const textRsenha = document.querySelector('.textRsenha')
 
-const campos =document.querySelector('.campos')
+const campos = document.querySelector('.campos')
 
 
 
@@ -21,60 +22,90 @@ formulario.addEventListener('submit', e => {
     console.log('Formuário não enviado')
 })
 
-formulario.addEventListener('submit',() =>{
+formulario.addEventListener('submit', () => {
 
-    if(campos.value === ''){
+    if (campos.value === '') {
         alert('Preencha os campos!')
-        
+
     }
     //NOME:
 
-    if(nomeInput.value === ''){
-            textName.innerHTML = 'invalido';
-            nomeInput.style.border = '1px solid red';
+    if (nomeInput.value === '') {
+        textName.innerHTML = 'invalido';
+        nomeInput.style.border = '1px solid red';
 
-        }else if(nomeInput.value.length >= 3 ){
-            nomeInput.style.border = '1px solid black';
-            textName.innerHTML = '';
+    } else if (nomeInput.value.length >= 3) {
+        nomeInput.style.border = '1px solid black';
+        textName.innerHTML = '';
 
-        }
-        else if(nomeInput.value.length < 3){
-            textName.innerHTML = 'pequeno'
-            nomeInput.style.border = '1px solid red';
-            
-        }
-        else{
-            textName.innerHTML = 'off';
-        }
+    }
+    else if (nomeInput.value.length < 3) {
+        textName.innerHTML = 'pequeno'
+        nomeInput.style.border = '1px solid red';
 
-        //SOBRENOME:
+    }
+    else {
+        textName.innerHTML = 'off';
+    }
 
-        if(sobrenomeInput.value === ''){
-            sobrenomeInput.style.border = '1px solid red';
+    //SOBRENOME:
 
-        }else if(sobrenomeInput.value.length >= 3 ){
-            sobrenomeInput.style.border = '1px solid black';
-            textSobrenome.innerHTML = '';
+    if (sobrenomeInput.value === '') {
+        sobrenomeInput.style.border = '1px solid red';
 
-        }
-        else if(sobrenomeInput.value.length < 3){
-            textSobrenome.innerHTML = 'pequeno';
-            sobrenomeInput.style.border = '1px solid red';
-            
-        }
-        else{
-            textSobrenome.innerHTML = '';
-        }
+    } else if (sobrenomeInput.value.length >= 3) {
+        sobrenomeInput.style.border = '1px solid black';
+        textSobrenome.innerHTML = '';
 
-        //CPF:
-        if(cpf.value.length < 11){
-            alert('pe')
-        }
-        
-    })
-        
-        
+    }
+    else if (sobrenomeInput.value.length < 3) {
+        textSobrenome.innerHTML = 'pequeno';
+        sobrenomeInput.style.border = '1px solid red';
 
-    
+    }
+    else {
+        textSobrenome.innerHTML = '';
+    }
 
-       
+    //CPF:
+    if (cpf.value.length < 11) {
+        cpf.style.border = '1px solid red';
+        textcpf.innerHTML = 'CPF invalido'
+    } else if (cpf.value.length > 14) {
+        textcpf.innerHTML = 'CPF invalido';
+    }
+
+
+    //SENHA:
+    if (senha.value === '') {
+        senha.style.border = '1px solid red';
+        textSenha.innerHTML = 'Senha invalida'
+    }else if(senha.value < 8){
+        textSenha.innerHTML = 'Senha muito curta'
+    } else if(senha != repetirSenha){
+        senha.style.border = '1px solid red'
+    }
+
+
+    if(senha != repetirSenha){
+        repetirSenha.style.border = '1px solid red'
+        textRsenha.innerHTML = 'senhas não coincidem'
+    }
+})
+
+cpf.addEventListener('keypress', () => {
+    let cpflength = cpf.value.length
+
+    if (cpflength === 3) {
+        cpf.value += '.'
+    } else if (cpflength === 7) {
+        cpf.value += '.'
+    } else if (cpflength === 11) {
+        cpf.value += '-'
+    }
+})
+
+
+
+
+
